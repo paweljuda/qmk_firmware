822d0091066a138d7d0e9663a96b8072160ec890
Jack Juda <jackskjuda@gmail.com>
2020-07-01 18:59:58 -0400

    Update keymap.c

--- keyboards/handwired/dactyl_manuform/5x6/keymaps/default/keymap.c
+++ keyboards/handwired/dactyl_manuform/5x6/keymaps/default/keymap.c
@@ -30,7 +30,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
   /* for gaming, it modifies left side only for match awsd movements */
   [_AWSD] = LAYOUT_5x6(
-     KC_GRAVE  , KC_1 ,      KC_2 , KC_3 , KC_4 , KC_5 ,                     KC_6 , KC_7 , KC_8    , KC_9   , KC_0    , KC_LBRC,
+     KC_ESC    , KC_1 ,      KC_2 , KC_3 , KC_4 , KC_5 ,                     KC_6 , KC_7 , KC_8    , KC_9   , KC_0    , KC_LBRC,
      KC_TAB    , KC_T ,      KC_Q , KC_W , KC_E , KC_R ,                     KC_Y , KC_U , KC_I    , KC_O   , KC_P    , KC_BSLASH ,
      LOWER     , KC_LSHIFT , KC_A , KC_S , KC_D , KC_F ,                     KC_H , KC_J , KC_K    , KC_L   , KC_SCLN , KC_QUOT   ,
      TG(_AWSD) , KC_B ,      KC_Z , KC_X , KC_C , KC_V ,                     KC_N , KC_M , KC_COMM , KC_DOT , KC_SLSH , KC_RBRC ,

6c53104dba1e46fbf97980bc28ddd6747a5df26b
jackskj <jackskjuda@gmail.com>
2022-02-06 19:39:06 -0500

    updates key bindings

--- keyboards/handwired/dactyl_manuform/5x6/keymaps/default/keymap.c
+++ keyboards/handwired/dactyl_manuform/5x6/keymaps/default/keymap.c
@@ -34,7 +34,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      KC_TAB    , KC_T ,      KC_Q , KC_W , KC_E , KC_R ,                     KC_Y , KC_U , KC_I    , KC_O   , KC_P    , KC_BSLASH ,
      LOWER     , KC_LSHIFT , KC_A , KC_S , KC_D , KC_F ,                     KC_H , KC_J , KC_K    , KC_L   , KC_SCLN , KC_QUOT   ,
      TG(_AWSD) , KC_B ,      KC_Z , KC_X , KC_C , KC_V ,                     KC_N , KC_M , KC_COMM , KC_DOT , KC_SLSH , KC_RBRC ,
-                       KC_LEFT, KC_RIGHT , KC_LSHIFT ,                            SFT_T(KC_ENT) , KC_DOWN , KC_UP ,
-                       KC_I , KC_O , KC_SPACE , KC_P ,                            KC_BSPC       , KC_SPACE , KC_MINS , KC_PLUS
+                       KC_LEFT, KC_RIGHT , KC_I ,                                   SFT_T(KC_ENT) , KC_DOWN , KC_UP ,
+                       KC_LCTRL, KC_I, KC_SPACE , KC_P ,                            KC_BSPC       , KC_SPACE , KC_MINS , KC_PLUS
   ),
 };
